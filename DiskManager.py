d1fb6feb28b4a5aa903aede6bfbf79ed53139079
quannguyen2153 <quannguyen2153@gmail.com>
2023-03-08 20:56:09 +0700

    Update DiskManager.py
    Read FAT32 RDET and get items

--- DiskManager.py
+++ DiskManager.py
@@ -1,4 +1,5 @@
 import wmi
+from queue import LifoQueue
 
 # Get USB physical drives
 def GetUSBDrive():
@@ -55,7 +56,10 @@ def ReadPhysicalDrive(driveName, sectorBytes):
             # 12 = 0x0C (FAT32)
             elif MBRPart["Type"] == 12:
                 bootSectorInfo = ReadFAT32BootSector(driveName, sectorBytes, MBRPart["LBABegin"])
-                ReadFAT32RDET(driveName, sectorBytes, MBRPart["LBABegin"] + bootSectorInfo["SectorsBeforeFAT"] + bootSectorInfo["FATTables"] * bootSectorInfo["FATSectors"])
+                RDETItems = ReadFAT32RDET(driveName, sectorBytes, MBRPart["LBABegin"] + bootSectorInfo["SectorsBeforeFAT"] + bootSectorInfo["FATTables"] * bootSectorInfo["FATSectors"])
+                
+                for item in RDETItems:
+                    PrintFAT32Item(item)
             else:
                 print("Unknown parition type") 
 
@@ -86,25 +90,99 @@ def ReadFAT32BootSector(driveName, sectorBytes, LBAbegin):
 
 # Read FAT32 RDET
 def ReadFAT32RDET(driveName, sectorBytes, sectorBegin):
+    RDETItems = []
     with open(driveName, "rb") as drive:
         drive.seek(sectorBegin * 32 * 16)
-        RDET = drive.read(sectorBytes)
+        while(True):            
+            RDET = drive.read(sectorBytes)
+            entryQueue = LifoQueue()
 
-        # Entry size is 32B
-        for i in range(0, sectorBytes, 32):
-            # Sub entry
-            # Entry
-            entry = {
-                "Name": RDET[i : i + 8],
-                "ExtendedName": RDET[i + int("08", 16) : i + int("08", 16) + 3],
-                "Attributes": "{0:08b}".format(RDET[i + int("0B", 16)]),
-                "TimeCreated": int.from_bytes(RDET[i + int("0D", 16) : i + int("0D", 16) + 3], "little"),
-                "DateCreated": int.from_bytes(RDET[i + int("10", 16) : i + int("10", 16) + 2], "little"),
-                "ClusterBegin": int.from_bytes(RDET[i + int("1A", 16) : i + int("1A", 16) + 2], "little"),
-                "Size": int.from_bytes(RDET[i + int("1C", 16) : i + int("1C", 16) + 4], "little"),
-            }
-            print(entry)
+            # Entry size is 32B
+            for i in range(0, sectorBytes, 32):
+                # Break the read while loop
+                if RDET[i] == 0:
+                    break
+                # Skip if deleted
+                if RDET[i] == 229: # 229 = 0xE5
+                    continue            
+                if RDET[i + int("0B", 16)] == 15: # 15 = 0x0F
+                # Sub entry
+                    subEntry = {
+                        "Name1": RDET[i + int("01", 16) : i + int("01", 16) + 10].decode("utf-16"),
+                        "Name2": RDET[i + int("0E", 16) : i + int("0E", 16) + 12].decode("utf-16"),
+                        "Name3": RDET[i + int("1C", 16) : i + int("1C", 16) + 4].decode("utf-16")
+                    }
+                    entryQueue.put(subEntry)
+                else:
+                # Entry
+                    # Get entry full name
+                    entryName = ""
+                    while not entryQueue.empty():
+                        subEntry = entryQueue.get()
+                        for j in range(1, 4):
+                            entryName += subEntry["Name" + str(j)]
+                    entryName = entryName[:entryName.find("\x00")]
 
+                    entry = {
+                        "Name": entryName,
+                        "PrimaryName": RDET[i : i + 8].decode("utf-8"),
+                        "ExtendedName": RDET[i + int("08", 16) : i + int("08", 16) + 3].decode("utf-8"),
+                        "Attributes": GetFAT32FileAttributes("{0:08b}".format(RDET[i + int("0B", 16)])),
+                        "TimeCreated": GetFAT32FileTimeCreated("".join(format(byte, '08b') for byte in RDET[i + int("0D", 16) : i + int("0D", 16) + 3][::-1])),
+                        "DateCreated": GetFAT32FileDateCreated("".join(format(byte, '08b') for byte in RDET[i + int("10", 16) : i + int("10", 16) + 2][::-1])),
+                        "ClusterBegin": int.from_bytes(RDET[i + int("1A", 16) : i + int("1A", 16) + 2], "little"),
+                        "Size": int.from_bytes(RDET[i + int("1C", 16) : i + int("1C", 16) + 4], "little")
+                    }
+                    RDETItems.append(entry)
+            else:
+                continue
+            break
+    
+    return RDETItems
+
+# Get FAT32 file attributes
+def GetFAT32FileAttributes(bitArray):
+    attributes = []
+    if bitArray[7] == "1":
+        attributes.append("ReadOnly")
+    if bitArray[6] == "1":
+        attributes.append("Hidden")
+    if bitArray[5] == "1":
+        attributes.append("System")
+    if bitArray[4] == "1":
+        attributes.append("VolLabel")
+    if bitArray[3] == "1":
+        attributes.append("Directory")
+    if bitArray[2] == "1":
+        attributes.append("Archive")
+    return attributes
+
+# Get FAT32 file time created
+def GetFAT32FileTimeCreated(bitArray):
+    return {
+        "Hour": int("".join(str(x) for x in bitArray[:5]), 2),
+        "Minute": int("".join(str(x) for x in bitArray[5:11]), 2),
+        "Second": int("".join(str(x) for x in bitArray[11:17]), 2),
+        "MiliSecond": int("".join(str(x) for x in bitArray[17:]), 2),
+    }
+
+# Get FAT32 file date created
+def GetFAT32FileDateCreated(bitArray):
+    return {
+        "Year": int("".join(str(x) for x in bitArray[:7]), 2) + 1980,
+        "Month": int("".join(str(x) for x in bitArray[7:11]), 2),
+        "Day": int("".join(str(x) for x in bitArray[11:]), 2)
+    }
+
+# Print FAT32 item
+def PrintFAT32Item(item):
+    print("{")
+    print("    Name:", item["Name"] if item["Name"] != "" else item["PrimaryName"] + item["ExtendedName"])
+    print("    Attributes:", item["Attributes"])
+    print("    Date Created:", item["DateCreated"])
+    print("    Time Created:", item["TimeCreated"])
+    print("    Size:", item["Size"])
+    print("}")
 
 # Test functions
 USBDrives = GetUSBDrive()
