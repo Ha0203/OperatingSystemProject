7c2919163a1e032635bf9e4fd7068a69e5af1b9d
ductruonghoc <lmduc21@clc.fitus.edu.vn>
2023-03-26 14:30:10 +0700

    UI 1.0

--- UIManager.py
+++ UIManager.py
@@ -2,61 +2,98 @@ from tkinter import *
 from tkinter import ttk
 import DiskManager
 
+def TreeDel():
+    for c in dirTreeview.get_children():
+        dirTreeview.delete(c)
+
+def FATBuild():
+    #Adding item to directory tree
+    TreeDel()
+    count = 0
+    for item in diskHierarchyF:
+        dirTreeview.insert("", END, text=item["Name"], iid=count, open=False)
+        item["ID"] = count
+        count += 1
+        
+    aCount = []
+    i = 0
+    while i < count:
+        aCount.append(0)
+        i += 1
+        
+    for item in diskHierarchyF:
+        if item["Parent"] >= 0:
+            dirTreeview.move(item["ID"], item["Parent"], aCount[item["Parent"]])
+            
+def NTFSBuild():
+    #Adding item to directory tree
+    TreeDel()
+    count = 0
+    for item in diskHierarchyN:
+        dirTreeview.insert("", END, text=item["Name"], iid=count, open=False)
+        count += 1
+        
+    aCount = []
+    i = 0
+    while i < count:
+        aCount.append(0)
+        i += 1
+        
+    for item in diskHierarchyN:
+        if item["Parent"] >= 0:
+            dirTreeview.move(item["ID"], item["Parent"], aCount[item["Parent"]])
 # Data
 diskPartitions = DiskManager.diskPartitions
-diskHierarchy = []
+diskHierarchyF = []
+diskHierarchyN = []
+c = 0
 for partition in diskPartitions:
-    if partition["Format"] == "NTFS":
-        diskHierarchy = partition["Hierarchy"]
+    if partition["Format"] == "FAT32":
+        diskHierarchyF = partition["Hierarchy"]
+        c += 1
+    elif partition["Format"] == "NTFS":
+        diskHierarchyN = partition[diskHierarchyN]
+        c += 1
+    if c == 2:
         break
 
 # GUI window
 window = Tk()
 
 window.title("Disk Check")
-
-window.configure(width=800, height=600, bg="#704684")
-
-windowName = Label(window, text="DISK CHECK", fg="black", font=(50), bg="#704684")
-windowName.place(anchor=N, x=400, y=10)
-
+window.configure(width=800, height=600, bg="#19A7CE")
+windowName = Label(window, text="DISK CHECK", fg="white", font=("MS Serif", 50, "bold"), bg="#19A7CE")
+windowName.grid(row = 0, column = 0, padx = 200)
 window.resizable(False, False)
 
-# Items List
-itemList = Canvas(window, bg="#323232", height=550, width=400)
-itemList.place(anchor=SW, x=0, y=600)
+#Content
+content = Canvas(window, bg="#ffffff", height=550, width=400)
+content.grid(row = 1, column = 0, sticky = NW)
 
+# Items List
+itemList = Canvas(content, bg="#ffffff", height=550, width=400)
+itemList.grid(row = 0, column = 0, sticky = NW)
+
+#Button Choice
+select1 = Button(itemList, text = "FAT32", bg = '#ffffff', relief = SOLID, borderwidth = 0, command = FATBuild)
+select1.grid(row = 0, column = 0, sticky = W, pady = 2)
+select2 = Button(itemList, text = "NTFS", bg = '#ffffff', relief = SOLID, borderwidth = 0, command = NTFSBuild)
+select2.grid(row = 0, column = 0)
 # Treeview
 dirTreeview = ttk.Treeview(itemList, height=24)
-dirTreeview.column("#0", width=390)
+dirTreeview.column("#0", width = 390)
 dirTreeview.heading('#0', text='FAT32', anchor=W)
+dirTreeview.grid(row = 1, column = 0)
 
-# Adding item to directory tree
-# idCount = 0
-# for item in diskHierarchy:  
-#     if item["Parent"] < 0:
-#         dirTreeview.insert("", END, text=item["Name"], iid=idCount, open=False)
-#     else:
-#         dirTreeview.insert(item["Parent"], END, text=item["Name"], iid=idCount, open=False)
-#     idCount += 1
+FATBuild()
 
-count = 0
-for item in diskHierarchy:
-    dirTreeview.insert("", END, text=item["Name"], iid=item["ID"], open=False)
-for item in diskHierarchy:
-    if item["Parent"] != item["ID"]:
-        dirTreeview.move(item["ID"], item["Parent"],count)
-    count += 1
-
-dirTreeview.place(anchor=NW, x=2, y=43)
-
-# Item Information
-itemInfo = Canvas(window, bg="#646E8F", height=550, width=400)
-itemInfo.place(anchor=SE, x=800, y=600)
+itemInfo = Canvas(content, bg="#646E8F", height=550, width=450)
+itemInfo.grid(row = 0, column = 1, sticky = NE)
 
 # Style
 style = ttk.Style()
 style.theme_use("clam")
 style.configure("Treeview", background="#323232", foreground="white", fieldbackground="#323232")
+
 
 window.mainloop()
